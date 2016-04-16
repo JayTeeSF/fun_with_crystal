732afcc3b037a6d23350f601c3d1e529b9c09f39
JayTeeSF <jonathan@his-service.net>
2016-04-16 16:17:46 -0700

    word counting program

---
+++ word_count.cr
@@ -0,0 +1,37 @@
+# crystal build --release word_count.cr
+# ./word_count ./word_count.cr
+class WordCount
+  def initialize(filename="")
+    @filename = filename || ""
+    puts("Unknown file: %s" % @filename) unless File.exists?(@filename)
+
+    @results = {"word" => 0}
+    @results.delete("word")
+    count_words
+  end
+
+  def print_counts
+    @results.to_a.sort{ |a, b| b.last <=> a.last }.each do |ary|
+      word, count = ary
+      puts "%d, %s" % [count, word]
+    end
+  end
+
+
+  private def count_words
+    return unless File.exists?(@filename)
+    File.open(@filename, "r") do |f|
+      f.each_line do |l|
+        l.split().each do |w|
+          @results[w] ||= 0
+          @results[w] += 1
+        end
+      end
+    end
+  end
+end
+
+# filename = (ARGV[0] || "").to_s
+filename = "./hi.cr"
+wc = WordCount.new(filename)
+wc.print_counts
